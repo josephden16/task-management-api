98dddba3b01ad818c66f6a04bdbe8ac98ac662df
Denedo Oghenetega <tegadev01@gmail.com>
2023-08-06 13:02:16 +0100

    fix: adjust the access token expiry time for development

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,10 +1,11 @@
 import { SignJWT, jwtVerify } from "jose";
+import { isDev } from "./env";
 
 export const refreshTokenDurationInMs = 60 * 60 * 24 * 30 * 1000;
 
 export const createAccessToken = (user: { id: string; email: string }) => {
   const iat = Math.floor(Date.now() / 1000);
-  const exp = iat + 60 * 15;
+  const exp = isDev() ? iat + 60 * 60 * 24 * 1 : iat + 60 * 15;
 
   return new SignJWT({ payload: { id: user.id, email: user.email } })
     .setProtectedHeader({ alg: "HS256", typ: "JWT" })
